5380ba12cd0d7124c4d3a258376c33b0918c3ecf
Sam Radhakrishnan <sk09idm@gmail.com>
2020-07-30 13:01:24 +0530

    First commit
    Add a binary executable with a command line parser.
    Add a parser to read config files and create rules.

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+#[macro_use]
+extern crate clap;
+extern crate csv;
+use clap::{App};
+use std::fs::File;
+use std::io::{self};
+use std::fmt;
+use serde::Deserialize;
+extern crate strum;
+#[macro_use] extern crate strum_macros;
+
+
+#[derive(Debug, Deserialize, EnumString, Display)]
+enum Event {
+    CREATE,
+    MODIFY,
+    DELETE
+}
+
+#[derive(Debug, Deserialize)]
+struct Rule {
+    event: Event,
+    file_pattern: String,
+    cmd: String,
+}
+
+
+impl fmt::Display for Rule {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} {}", self.event, self.file_pattern, self.cmd)
+    }
+}
+
+fn parse_rules(config_path: String) -> Result<Vec<Rule>, io::Error> {
+    let file = File::open(&config_path)?;
+    let mut csv_file = csv::ReaderBuilder::new().has_headers(false).delimiter(b'\t').from_reader(file);
+
+    let mut rules: Vec<Rule> = Vec::new();
+    for record in csv_file.deserialize() {
+        let rule: Rule = record?;
+        rules.push(rule);
+    }
+    Ok(rules)
+}
+
+
+fn watch_files(rules: Vec<Rule>, time_in_seconds: u64, dir: String) -> Result<(), io::Error> {
+    unimplemented!();
+}
+
+fn main() {
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let config = matches.value_of("config").unwrap_or("/home/sam/default.conf");
+    let time = matches.value_of("time").unwrap_or("5");
+    let dir = matches.value_of("file").unwrap();
+
+    println!("Config: {}, File {}, Time {}", config, dir, time);
+
+    let time_in_seconds: u64 = time.parse().unwrap();
+
+    let rules = parse_rules(config.to_string()).expect("error reading config file");
+    watch_files(rules, time_in_seconds, dir.to_string());
+}
